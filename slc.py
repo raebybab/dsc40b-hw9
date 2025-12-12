20be9d53f0de9f641605fc6c82c7dbf3431921f8
Rose Park <seyeonp7@gmail.com>
2025-12-11 20:19:20 -0800

    add the class

--- slc.py
+++ slc.py
@@ -1,28 +1,112 @@
-from disjoint_set_forest import DisjointSetForest
+class DisjointSetForest:
+    def __init__(self, elements):
+        self._core = _DisjointSetForestCore()
+        self.element_to_id = {}
+        self.id_to_element = {}
+
+        for element in elements:
+            eid = self._core.make_set()
+            self.element_to_id[element] = eid
+            self.id_to_element[eid] = element
+
+    def find_set(self, element):
+        """Return the representative of the set containing element."""
+        return self.id_to_element[
+            self._core.find_set(self.element_to_id[element])
+        ]
+
+    def union(self, x, y):
+        """Union the sets containing x and y."""
+        x_id = self.element_to_id[x]
+        y_id = self.element_to_id[y]
+        self._core.union(x_id, y_id)
+
+    def in_same_set(self, x, y):
+        """Return True if x and y belong to the same set."""
+        return self.find_set(x) == self.find_set(y)
+
+
+class _DisjointSetForestCore:
+    def __init__(self):
+        self._parent = []
+        self._rank = []
+        self._size_of_set = []
+
+    def make_set(self):
+        """Create a new singleton set, return its id."""
+        x = len(self._parent)
+        self._parent.append(None)
+        self._rank.append(0)
+        self._size_of_set.append(1)
+        return x
+
+    def find_set(self, x):
+        """Path compression find."""
+        parent = self._parent[x]
+        if parent is None:
+            return x
+        root = self.find_set(parent)
+        self._parent[x] = root
+        return root
+
+    def union(self, x, y):
+        """Union by rank."""
+        x_rep = self.find_set(x)
+        y_rep = self.find_set(y)
+
+        if x_rep == y_rep:
+            return
+
+        if self._rank[x_rep] > self._rank[y_rep]:
+            self._parent[y_rep] = x_rep
+            self._size_of_set[x_rep] += self._size_of_set[y_rep]
+        else:
+            self._parent[x_rep] = y_rep
+            self._size_of_set[y_rep] += self._size_of_set[x_rep]
+            if self._rank[x_rep] == self._rank[y_rep]:
+                self._rank[y_rep] += 1
+
+
+# ================================
+# Single Linkage Clustering (Kruskal-based)
+# ================================
 
 def slc(graph, d, k):
+    """
+    Perform single linkage clustering on an undirected graph using Kruskal’s algorithm.
+    Return a frozenset of k frozensets, each representing a cluster.
+    """
+
+    # All nodes in the graph
     nodes = list(graph.nodes)
+
+    # Initialize DSF with each node in its own set
     dsf = DisjointSetForest(nodes)
-
-    edges = []
-    for u, v in graph.edges:
-        edges.append((d((u, v)), u, v))
-
-    edges.sort(key=lambda x: x[0])
-
     num_clusters = len(nodes)
 
-    for dist, u, v in edges:
+    # Build list of weighted edges: (weight, u, v)
+    weighted_edges = []
+    for u, v in graph.edges:
+        weighted_edges.append((d((u, v)), u, v))
+
+    # Sort edges by weight (ascending)
+    weighted_edges.sort(key=lambda x: x[0])
+
+    # Kruskal-like merging until only k clusters remain
+    for weight, u, v in weighted_edges:
         if num_clusters == k:
             break
-
         if not dsf.in_same_set(u, v):
             dsf.union(u, v)
             num_clusters -= 1
 
+    # Build clusters by grouping nodes with the same representative
     clusters = {}
     for node in nodes:
-        root = dsf.find_set(node)
-        clusters.setdefault(root, set()).add(node)
+        rep = dsf.find_set(node)
+        if rep not in clusters:
+            clusters[rep] = set()
+        clusters[rep].add(node)
 
+    # Convert to required output format
     return frozenset(frozenset(cluster) for cluster in clusters.values())
